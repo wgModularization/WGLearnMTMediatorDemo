3e259c79daa37d21b26938583ba40697383e35aa
wanggang <wxwangg@163.com>
2018-04-27 15:16:26 +0800

    Initial Commit

---
+++ WGLearnMTMediatorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGLearnMTMediatorDemo
+//
+//  Created by wanggang on 2018/4/27.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WGLearnMTMediatorDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WGLearnMTMediatorDemo
+//
+//  Created by wanggang on 2018/4/27.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
